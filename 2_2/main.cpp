5b12ef2008228dd8a3df782664445635fb27723c
Frollerman <frollerman@yandex.ru>
2021-09-19 23:06:29 +0300

    first commit

---
+++ 2_2/main.cpp
@@ -0,0 +1,27 @@
+#include <QObject>
+
+class MyClass : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(bool readOnly READ isReadOnly WRITE setReadOnly)
+
+private:
+    bool m_bReadOnly;
+
+public:
+    MyClass(QObject* pobj = 0) : QObject(pobj)
+                               , m_bReadOnly(false)
+    {
+    }
+
+public:
+    void setReadOnly(bool bReadOnly)
+    {
+        m_bReadOnly = bReadOnly;
+    }
+
+    bool isReadOnly() const
+    {
+        return m_bReadOnly;
+    }
+};
