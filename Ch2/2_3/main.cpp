5ef0e2ca8de1ef9a746227fbcfc2e526cfa3119b
Frollerman <frollerman@yandex.ru>
2021-09-20 15:09:19 +0300

    Ch2 Done

--- Ch2/2_3/main.cpp
+++ Ch2/2_3/main.cpp
@@ -150,3 +150,92 @@ MyClass::MyClass() : QObject()
 connect(pcmd, &QPushButton::clicked, [=](){pwgt->hide();});
 
 ///////////////////////////////////////////////////////////
+
+QObject::disconnect(sender, signal, receiver, slot);
+
+///////////////////////////////////////////////////////////
+
+void main()
+{
+    //...
+    QObject::disconnect(pSender, SIGNAL(signalMethod()),
+                        pReceiver, SLOT(slotMethod())
+                       );
+    //...
+}
+
+//disconnect(signal, receiver, slot)
+//disconnect(receiver, slot)
+
+///////////////////////////////////////////////////////////
+
+MyClass::MyClass(QWidget* pwgt)
+{
+    //...
+    QSignalMapper* psigMapper = new QSignalMapper(this);
+    connect(psigMapper, SIGNAL(mapped(const QString&)),
+            this,       SLOT(slotShowAction(const QString&))
+           );
+
+    QPushButton* pcmd1 = new QPushButton("Button1");
+    connect(pcmd1, SIGNAL(clicked()), psigMapper, SLOT(map()));
+    psigMapper->setMapping(pcmd1, "Button1 Action");
+
+    QPushButton* pcmd2 = new QPushButton("Button2");
+    connect(pcmd2, SIGNAL(clicked()), psigMapper, SLOT(map()));
+    psigMapper->setMapping(pcmd1, "Button2 Action");
+    //...
+}
+
+void MyClass::slotShowAction(const QString& str)
+{
+    qDebug() << str;
+}
+
+//////////////////////////////////////////////////////////class hierarchy
+
+QObject* pobj1 = new QObject;
+QObject* pobj2 = new QObject(pobj1);
+QObject* pobj4 = new QObject(pobj2);
+QObject* pobj3 = new QObject(pobj1);
+pobj2->setObjectName("the first child of pobj1");
+pobj3->setObjectName("the second child of pobj1");
+pobj4->setObjectName("the first child of pobj2");
+
+///////////////////////////////////////////////////////////object parent name output
+
+for (QObject* pobj = pobj4; pobj; pobj = pobj->parent())
+{
+    qDebug() << pobj->objectName();
+}
+
+QObject* pobj = pobj1->findChild<QObject*>("the first child of pobj2");
+
+QList<QObject*> plist = pobj1->findChildren<QObject*>(QRegExp("th*"));
+
+QList<QObject*> plist = pobj1->findChildren<QObject*>();
+
+//dumpObjectInfo(); //get name, parent class and signal-slot connections
+
+//dumpObjectTree(); //show children objects' hierarchy
+
+///////////////////////////////////////////////////////////
+
+qDebug() << pobj->metaObject()->className(); // get parent name from meta
+
+if (pobj1->metaObject()->className() == "MyClass") //compare class name with known
+{
+    //Do something
+}
+
+if(pobj->inherits("QWidjet")) //get class inheritance info
+{
+    QWidget* pwgt = static_cast<QWidget*>(pobj);
+    //Do something with pwgt
+}
+
+QWidget* pwgt = qobject_cast<QWidget*>(pobj);
+if(pwgt)
+{
+    //Do something with pwgt
+}
