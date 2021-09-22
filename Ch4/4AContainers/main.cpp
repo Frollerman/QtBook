6a2adbd1a31565e28fb0387bd2f0d72262453f15
Frollerman <frollerman@yandex.ru>
2021-09-22 20:11:45 +0300

    Added iterators and containers

---
+++ Ch4/4AContainers/main.cpp
@@ -0,0 +1,88 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QSet>
+#include <QtCore>
+//-------------------------------------------QHash and QMultiHash
+
+int main(int argc, char *argv[])
+{
+    //-----Associative Containers----------------------
+
+    //------------------------------------------QMap and QMultiMap
+
+    QMap<QString, QString> mapPhonebook;
+    mapPhonebook["Piggy"] = "+49 631322187";
+    mapPhonebook["Kermit"] = "+49 631322181";
+    mapPhonebook["Gonzo"] = "+49 631322186";
+
+    QMap<QString, QString>::iterator it = mapPhonebook.begin();
+    for (;it != mapPhonebook.end(); ++it)
+    {
+        qDebug() << "Name:" << it.key()
+                 << " Phone:" << it.value();
+    }
+
+    if (mapPhonebook.contains("Kermit"))
+    {
+        qDebug() << "Phone:" << mapPhonebook["Kermit"];
+    }
+
+    QMultiMap<QString, QString> multiMapPhonebook;
+    multiMapPhonebook.insert("Kermit", "+49 631322181");
+    multiMapPhonebook.insert("Gonzo", "+49 631322186");
+    multiMapPhonebook.insert("Gonzo", "+49 631322000");
+    multiMapPhonebook.insert("Gonzo", "+49 631322010");
+    multiMapPhonebook.insert("Piggy", "+49 631322187");
+    multiMapPhonebook.insert("Piggy", "+49 631322999");
+
+    QMultiMap<QString, QString>::iterator mit =
+                                    multiMapPhonebook.find("Piggy");
+    for (; mit != multiMapPhonebook.end() && mit.key() == "Piggy"; ++mit)
+    {
+        qDebug() << mit.value();
+    }
+
+//    class MyClass
+//    {
+//    public:
+//        QString firstName;
+//        QString secondName;
+//    };
+
+//    inline bool operator==(const MyClass& mc1, const MyClass& mc2)
+//    {
+//        return (mc1.firstName() == mc2.firstName()
+//                && mc1.secondName() == mc2.secondName()
+//                );
+//    }
+
+//    inline uint qHash(const MyClass& mc)
+//    {
+//        return qHash(mc.firstName()) ^ qHash(mc.secondName());
+//    }
+
+    //-----------------------------------------------------QSet
+
+    QSet<QString> set1;
+    QSet<QString> set2;
+
+    set1 << "Therion" << "Nightwish" << "Xandria";
+    set2 << "Mantus" << "Haggard" << "Therion";
+
+    QSet<QString> setResult = set1;
+    setResult.unite(set2);
+    qDebug() << "Unity = " << setResult;
+
+    setResult = set1;
+    setResult.intersect(set2);
+    qDebug() << "Intersection of set1 with set2 = " << setResult;
+
+    setResult = set1;
+    setResult.subtract(set2);
+    qDebug() << "Subtraction of set2 from set1 = " << setResult;
+
+
+
+    return 0;
+}
--- Ch4/4Containers/main.cpp
+++ Ch4/4Containers/main.cpp
@@ -1,8 +1,75 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QBitArray>
+#include <QByteArray>
+#include <QtCore>
 
 int main(int argc, char *argv[])
 {
+    //----------------------------------------QVector
 
+    QVector<int> vec;
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+
+    qDebug() << vec;
+
+    QByteArray arr[3];
+//    arr[0] = arr[1] = 0xFF;
+//    arr[0] = 0xFF;
+//    arr[1] = 0xFF;
+//    arr[2] = 0x0;
+
+    QByteArray a =           "Test Data";
+    QByteArray aCompressed = qCompress(a);
+    qDebug() << qUncompress(aCompressed);
+
+    QByteArray aBase64 = a.toBase64();
+    qDebug() << aBase64;
+
+    qDebug() << QByteArray::fromBase64(aBase64);
+
+    QBitArray bits(3);
+    bits[0] = bits[1] = true;
+    bits[2] = false;
+
+    //------------------------------------------QList
+
+    QList<int> list;
+    list << 10 << 20 << 30;
+
+    QList<int>::iterator it = list.begin();
+    while (it != list.end())
+    {
+        qDebug() << "Element:" << *it;
+        ++it;
+    }
+
+    //-------------------------------------------QStack
+
+    QStack<QString> stk;
+    stk.push("Era");
+    stk.push("Corvus Corax");
+    stk.push("Gathering");
+
+    while (!stk.empty())
+    {
+        qDebug() << "Element:" << stk.pop();
+    }
+
+    //-------------------------------------------QQueue
+
+    QQueue<QString> que;
+    que.enqueue("Era");
+    que.enqueue("Corvus Corax");
+    que.enqueue("Gathering");
+
+    while (!que.empty())
+    {
+        qDebug() << "Element:" << que.dequeue();
+    }
+
+    return 0;
 }
