17d976acce7b831f92fe1f2da4c1aa04c89906cc
Frollerman <frollerman@yandex.ru>
2021-09-23 14:47:45 +0300

    Finished algos

--- Ch4/4Algo/main.cpp
+++ Ch4/4Algo/main.cpp
@@ -15,6 +15,8 @@ bool caseInsensitiveLessThan(const QString &s1, const QString &s2)
 
 int main(int argc, char *argv[])
 {
+    //-------------------------------------------------------Sort
+
     QString values[] = {"Xandria", "Therion", "Nightwish", "Haggard"};
     const int n = sizeof(values) / sizeof(QString);
     QString copyOfValues[n];
@@ -39,6 +41,55 @@ int main(int argc, char *argv[])
     list3 << "Within Temptation" << "Anubis" << "anubis" << "Mantus";
     std::sort(list3.begin(), list3.end(), lessThan);
     qDebug() << list3;
+
+    //-----------------------------------------------Search
+
+    QList<QString> list4;
+    list4 << "Within Temptation" << "Anubis" << "Mantus";
+    QList<QString>::iterator it =
+            std::find(list4.begin(), list4.end(), "Anubis");
+    if (it != list.end())
+    {
+        qDebug() << "Found=" << *it;
+    }
+    else
+    {
+        qDebug() << "Not Found";
+    }
+
+    //----------------------------------------------Comparison
+
+    QList<QString> list5;
+    list5 << "Within Temptation" << "Anubis" << "Mantus";
+
+    QVector<QString> vec(3);
+    vec[0] = "Within Temptation";
+    vec[1] = "Anubis";
+    vec[2] = "Mantus";
+
+    qDebug() << "Equal="
+             << std::equal(list5.begin(), list5.end(), vec.begin());
+
+    //---------------------------------------------Filling
+
+    QList<QString> list6;
+    list6 << "Within Temptation" << "Anubis" << "Mantus";
+    std::fill(list6.begin(), list6.end(), "Beatles");
+    qDebug() << list6;
+
+    //--------------------------------------------Copy
+
+    QList<QString> list7;
+    list7 << "Within Temptation" << "Anubis" << "Mantus";
+    QVector<QString> vec2(3);
+    std::copy(list7.begin(), list7.end(), vec2.begin());
+    qDebug() << vec2;
+
+    //----------------------------------------Counting
+    QList<QString> list8;
+    list8 << "Within Temptation" << "Anubis" << "Mantus" << "Mantus";
+    int num = std::count(list8.begin(), list8.end(), "Mantus");
+    qDebug() << num;
 
 
 
