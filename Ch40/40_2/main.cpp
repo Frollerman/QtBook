b456f301a0032e6c2ed28964927f1e37ef513cf3
Frollerman <frollerman@yandex.ru>
2021-11-07 13:21:13 +0300

    Started Ch40

---
+++ Ch40/40_2/main.cpp
@@ -0,0 +1,50 @@
+#include <QCoreApplication>
+#include <QtXml>
+
+// --------------------------------------------------------------
+
+void traverseNode(const QDomNode& node)
+{
+    QDomNode domNode = node.firstChild();
+    while(!domNode.isNull())
+    {
+        if(domNode.isElement())
+        {
+            QDomElement domElement = domNode.toElement();
+            if(!domElement.isNull())
+            {
+                if(domElement.tagName() == "contact")
+                {
+                    qDebug() << "Attr: "
+                             << domElement.attribute("number", "");
+                }
+                else
+                {
+                    qDebug() << "TagName: " << domElement.tagName()
+                             << "\tText: " << domElement.text();
+                }
+            }
+        }
+        traverseNode(domNode);
+        domNode = domNode.nextSibling();
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QDomDocument domDoc;
+    QFile file("addressbook.xml");
+
+    if(file.open(QIODevice::ReadOnly))
+    {
+        if(domDoc.setContent(&file))
+        {
+            QDomElement domElement = domDoc.documentElement();
+            traverseNode(domElement);
+        }
+        file.close();
+    }
+
+    return a.exec();
+}
